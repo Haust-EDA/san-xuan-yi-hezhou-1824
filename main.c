fef049949a7052ac993ec298377ad30d7c264ee7
hezhou-1824 <3969200452@qq.com>
2024-10-31 14:29:22 +0800

    first commit

--- main.c
+++ main.c
@@ -1,5 +1,14 @@
 //请在下方输入你的代码：
-int main()
+#include <stdio.h>
+#include <math.h>
+int   main( )
+
 {
-    return 0;
+	float a,b,c,x1,x2;
+	scanf("%f %f %f",&a,&b,&c);
+	x1=(-b+sqrt(b*b-4*a*c))/(2*a);
+	x2=(-b-sqrt(b*b-4*a*c))/(2*a);
+	printf("x1=%lf,x2=%lf",x1,x2);
+   return 0;
+
 }
